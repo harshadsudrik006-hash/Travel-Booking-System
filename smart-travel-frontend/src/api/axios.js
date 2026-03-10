ee7a954073648561588e6c1f17d552361a50002b
harshadsudrik006-hash <harshadsudrik006@gmail.com>
2026-03-10 09:39:04 +0530

    render deploy

--- smart-travel-frontend/src/api/axios.js
+++ smart-travel-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://smart-travel-task6.onrender.com/api",
+  baseURL: "https://smart-travel-task6-3.onrender.com/api",
   headers: {
     "Content-Type": "application/json"
   }
